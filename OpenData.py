bca8bc4aaf2e454f69b9c4e69e23572466e6cdff
hildeingvildhummel <58852349+hildeingvildhummel@users.noreply.github.com>
2023-10-14 20:54:11 +0200

    Update OpenData.py
    Created the total Kalman Filter and cleaned the code a bit

--- OpenData.py
+++ OpenData.py
@@ -1,7 +1,7 @@
-import cmath 
+import cmath
 import scipy.io
 import numpy as np
-# from Kalman import KalmanFilter_all_pilots
+from Kalman import One_Kalman_filter, Kalman_filter_per_channel
 import matplotlib.pyplot as plt
 from scipy.special import j0
 
@@ -66,135 +66,16 @@ def extract_all_symbols(freq_domain_signals, matfile):
         symbols.append(symbol)
     return np.array(symbols), c
 
-matfile = scipy.io.loadmat('./DataSet_OFDM/New_DataSet/DataSet2.mat')
+matfile = scipy.io.loadmat('../DataSet_OFDM/New_DataSet/DataSet2.mat')
 
-No_Noise_signal = matfile['HighNoise_RxSignal']
+No_Noise_signal = matfile['NoNoise_RxSignal']
+variance_w = 0 # 0 for no noise, -25 dB for low noise and -10 dB for high noise)
 
-# matfile = scipy.io.loadmat('./DataSet_OFDM/New_DataSet/DataSet1.mat')
-#
-# No_Noise_signal = matfile['HighNoise_RxSignal']
 
 frequency_domain_signals = to_frequency_domain_subsequences(No_Noise_signal)
 pilot_symbols, pilot_indices = extract_pilot_symbols(frequency_domain_signals, matfile)
 data_symbols, data_indices = extract_data_symbols(frequency_domain_signals, matfile)
 all_symbols, all_indices = extract_all_symbols(frequency_domain_signals, matfile)
-# KalmanFilter_all_pilots(pilot_symbols)
 
-np.set_printoptions(threshold=100)
-# symbols =  7 symbos for each carrier
-print("Pilot symbols, data symbols, data indices, pilot indices ", pilot_symbols.shape, data_symbols.shape, np.shape(data_indices), np.shape(pilot_indices))
-
-
-test = 1/1j
-if test != -1j:
-    raise Exception("Not equal to -1j")
-
-transmited_pilot = 0.707 + 0.707j
-Sigma_n = np.zeros((7,200), dtype=np.complex128)
-Sigma_prev = np.zeros(200, dtype=np.complex128) # We kunnen dit denk ik beter initialiseren
-A = np.zeros((7,200), dtype=np.complex128) # Wat is P ten opzichte van Sigma -> A?
-C = np.zeros((7,200), dtype=np.complex128)
-M_n = np.zeros((7,200), dtype=np.complex128)
-lambda_ = np.zeros((7,200), dtype=np.complex128)
-K = np.zeros((7,200), dtype=np.complex128)
-xn = np.zeros((7,200), dtype=np.complex128)
-# variance_Z = np.zeros((7,200), dtype=np.complex128)
-
-
-print("transmited pilot", transmited_pilot)
-####
-pilot = 0.707 + 0.707j
-
-variance_w = 0.1 # 0 for no noise, -25 dB for low noise and -10 dB for high noise)
-T, fd = (2048+160)/30720000, 50
-auto_correlation = [j0(2*np.pi*fd * 0 * T), j0(2*np.pi*fd * 1 *T)] # By equation from paper
-Sigma_prev = np.full((200),(auto_correlation[0]**2 - abs(auto_correlation[1])**2) / auto_correlation[0])
-
-a = auto_correlation[1] / auto_correlation[0]
-a = -a
-C = a
-
-print("Sigma_prev", Sigma_prev)
-
-xn_prev = np.full((200) ,0)
-g = 1 # Not correct yet?
-
-for t, symbols_at_T in enumerate(pilot_symbols):
-    
-    M_n[t,:] = C * Sigma_prev * np.conjugate(C) + 1
-    if M_n[t,:].shape != (200,):
-        raise Exception(f"M_N at T: {M_n[t,:].shape}")
-    
-    lambda_[t,:] = pilot * M_n[t,:] * np.conjugate(pilot) + variance_w
-
-    K[t,:] = M_n[t,:] * np.conjugate(pilot) / lambda_[t,:]
-
-    if symbols_at_T.shape != (200,):
-        raise Exception(f"Symbols at T: {symbols_at_T.shape}")
-    
-    xn[t,:] = C*xn_prev + K[t,:] * (symbols_at_T - pilot*C* xn_prev)
-    Sigma_n[t,:] = (1- K[t,:] * pilot) * M_n[t,:]
-    xn_prev = xn[t,:]
-    Sigma_prev = Sigma_n[t,:]
-
-
-print("M_n", M_n[0,:])
-print("lambda_", lambda_[0,:])
-print("K", K[0,:])
-print("xn", xn[0,:])
-print("Sigma_n", Sigma_n[0,:])
-fig_1, axs_1 = plt.subplots(4, figsize=(10, 8))
-im0 = axs_1[0].imshow(abs(K), aspect='auto')
-im1 = axs_1[1].imshow(abs(M_n), aspect='auto')
-im2 = axs_1[2].imshow(abs(lambda_), aspect='auto')
-im3 = axs_1[3].imshow(abs(Sigma_n), aspect='auto')
-
-axs_1[0].set_title('Kalman filter coefficients')
-axs_1[1].set_title('M_n')
-axs_1[2].set_title('lambda')
-axs_1[3].set_title('Sigma_n')
-fig_1.colorbar(im0, ax=axs_1[0], shrink=0.8)
-fig_1.colorbar(im1, ax=axs_1[1], shrink=0.8)
-fig_1.colorbar(im2, ax=axs_1[2], shrink=0.8)
-fig_1.colorbar(im3, ax=axs_1[3], shrink=0.8)
-
-fig_2, axs_2 = plt.subplots(3,3, figsize=(10, 8))
-
-im4 = axs_2[0, 0].imshow(abs(data_symbols[:,::5]/xn[:]), aspect='auto')
-im5 = axs_2[1, 0].imshow(np.angle(data_symbols[:,::5]/xn[ :]))
-im6 = axs_2[2, 0].imshow(abs(xn), aspect='auto')
-im7 = axs_2[0, 1].imshow(np.angle(xn), aspect='auto')
-im8 = axs_2[1, 1].imshow(abs(data_symbols), aspect='auto' )
-im9 = axs_2[2, 1].imshow(np.angle(data_symbols), aspect='auto')
-im10 = axs_2[0, 2].imshow(abs(pilot_symbols), aspect='auto')
-im11 = axs_2[1, 2].imshow(np.angle(pilot_symbols), aspect='auto')
-
-axs_2[0, 0].set_title('|data_symbols[:,::5]/xn[:]|')
-axs_2[1, 0].set_title('arg(data_symbols[:,::5]/xn[:])')
-axs_2[2, 0].set_title('|xn|')
-axs_2[0, 1].set_title('arg(xn)')
-axs_2[1, 1].set_title('|data_symbols|')
-axs_2[2, 1].set_title('arg(data_symbols)')
-axs_2[0, 2].set_title('|pilot_symbols|')
-axs_2[1, 2].set_title('arg(pilot_symbols)')
-
-fig_2.colorbar(im4, ax=axs_2[0, 0], shrink=0.8, aspect=40)
-fig_2.colorbar(im5, ax=axs_2[1, 0], shrink=0.8, aspect=40)
-fig_2.colorbar(im6, ax=axs_2[2, 0], shrink=0.8, aspect=40)
-fig_2.colorbar(im7, ax=axs_2[0, 1], shrink=0.8, aspect=40)
-fig_2.colorbar(im8, ax=axs_2[1, 1], shrink=0.8, aspect=40)
-fig_2.colorbar(im9, ax=axs_2[2, 1], shrink=0.8, aspect=40)
-fig_2.colorbar(im10, ax=axs_2[0, 2], shrink=0.8, aspect=40)
-fig_2.colorbar(im11, ax=axs_2[1, 2], shrink=0.8, aspect=40)
-
-for ax in axs_1.flat:
-
-    ax.set_ylabel('Time')
-    ax.set_ylim([0, data_symbols.shape[0]])
-
-for ax in axs_2.flat:
-   
-    ax.set_ylabel('Time')
-    ax.set_ylim([0, data_symbols.shape[0]])
-
-plt.show()
+h_n_single = Kalman_filter_per_channel(pilot_symbols, variance_w, data_symbols, plot=False)
+h_n_total = One_Kalman_filter(pilot_symbols, variance_w, data_symbols, plot=False)
